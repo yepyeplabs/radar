3e44f9f01ff3f77acf814feaa984e3cf3291263e
yepyeplabs <89113968+yepyeplabs@users.noreply.github.com>
2022-12-13 04:58:32 +0000

    adds alternative usage to .env file

--- src/components/TheMap.js
+++ src/components/TheMap.js
@@ -2,10 +2,11 @@
 import { useMemo } from "react"
 import Map, { GeolocateControl, Source, Layer } from "react-map-gl";
 import { notionToGeoJSON } from "../data"
+import {ENV} from "../env/env"
 import "./TheMap.css"
 
 const TheMap: React.FC = () => {
-  console.log(notionToGeoJSON())
+  
   const layerStyle = {
     id: 'point',
     type: 'circle',
@@ -21,11 +22,12 @@ const TheMap: React.FC = () => {
   }, [notionToGeoJSON()]);
 
   // get users location to center on that on first load.
+  console.log(ENV.REACT_APP_MAP_KEY)
   
   return (
      <Map
-        style={{height: '100vh', width: '100vw'}}
-       mapboxAccessToken=""
+       style={{height: '100vh', width: '100vw'}}
+       mapboxAccessToken={ENV.REACT_APP_MAP_KEY}
        initialViewState={{
           longitude: -100,
           latitude: 40,
---
+++ src/env/env.prod.js
@@ -0,0 +1,3 @@
+export const ENV = {
+  REACT_APP_MAP_KEY: 'pk.eyJ1IjoiaXJhc2FuY2hleiIsImEiOiJjbGIwYjlnbXUwMjJsM25xeHE5aWgweG1rIn0.CEpTV5ORGdMkiOetrgmcIA'
+}
